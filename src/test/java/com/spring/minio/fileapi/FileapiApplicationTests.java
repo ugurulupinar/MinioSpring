62ff8c60e7e22bd7ef89f463554b11226fd42a17
ugurulupinar <ugur.ulupinar2102@gmail.com>
2023-05-14 13:45:50 +0300

    first commit

---
+++ src/test/java/com/spring/minio/fileapi/FileapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.minio.fileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
